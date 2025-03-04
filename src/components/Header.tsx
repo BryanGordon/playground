2da7882ce04507b0db70184fbef4d6bb76967806
Bryan Gordon <bryanbge92@gmail.com>
2025-03-03 20:44:36 -0500

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../css/HeaderStyles.css'
+import { Navbar } from './Navbar'
+
+export function Header () {
+  return (
+    <header>
+      <h1>Playground</h1>
+      <Navbar />
+    </header>
+  )
+}
