d38875bc5b12aa347c68a009debf39b919413abd
Bryan Gordon <bryanbge92@gmail.com>
2025-03-03 20:44:53 -0500

    Create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import '../css/NavbarStyles.css'
+
+export function Navbar () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          Something 1
+        </li>
+        <li>
+          Something 2
+        </li>
+      </ul>
+    </nav>
+  )
+}
