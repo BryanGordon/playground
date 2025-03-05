018ff2cb29867d26f8bef72c37e37ce52692c447
Bryan Gordon <bryanbge92@gmail.com>
2025-03-04 22:33:54 -0500

    Add icon

---
+++ src/icons/CloseCross.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  handleChange: () => void
+}
+
+export const Cross = (handleChange) => {
+  return (
+    <svg  
+      width="24"  
+      height="24"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+      id='close-cross-icon'
+      onClick={() => handleChange}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </svg>
+  )
+}
