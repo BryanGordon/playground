c291c1c4f9fbe021492223111505a4720bf0dfed
Bryan Gordon <bryanbge92@gmail.com>
2025-03-04 22:32:07 -0500

    Update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Content } from './components/Content'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
+import { Modal } from './components/Modal'
 
 function App () {
   return (
     <main>
       <Header />
+      <Content />
+      <Modal />
+      <Content />
+      <Content />
       <Footer />
     </main>
   )
