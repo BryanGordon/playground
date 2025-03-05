13eea4f27e9263a0d5f8e5166cfe9282aabd2c3c
Bryan Gordon <bryanbge92@gmail.com>
2025-03-04 22:40:28 -0500

    Change tag

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,9 +13,9 @@ export function Modal () {
   return (
     <article className='container-modal'>
       <div className='container-info-modal'>
-        <div className='close-icon'>
-          <Cross handleChange={() => handleChange} />
-        </div>
+        <button className='close-icon' onClick={() => handleChange()}>
+          <Cross />
+        </button>
         <h2>Modal title</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quos temporibus nobis id vero velit quasi accusamus recusandae perferendis aliquid?</p>
       </div>
