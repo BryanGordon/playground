2dffeb75d144e3ddc4574b84bce8b3098851d059
Bryan Gordon <bryanbge92@gmail.com>
2025-03-06 23:21:05 -0500

    Update component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
-import '../css/HeaderStyles.css'
+// import '../css/HeaderStyles.css'
+import '../css/StaticNavbar.css'
+
 import { Navbar } from './Navbar'
 
 export function Header () {
   return (
     <header>
-      <h1>Playground</h1>
+      <h1 id='inicio'>Playground</h1>
       <Navbar />
     </header>
   )
