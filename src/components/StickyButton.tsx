0536c32a5e80a9b98eba9743d2c93c2d745dd527
Bryan Gordon <bryanbge92@gmail.com>
2025-03-06 23:21:20 -0500

    Create button

---
+++ src/components/StickyButton.tsx
@@ -0,0 +1,10 @@
+import '../css/StickyButton.css'
+import { Arrow } from '../icons/ArrowHome'
+
+export function StickyButton () {
+  return (
+    <a href='/' className='home-button'>
+      <Arrow />
+    </a>
+  )
+}
