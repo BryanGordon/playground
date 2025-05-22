d1f29ed30046e50a91c55dcf5ad41324c1b66bc7
Bryan Gordon <bryanbge92@gmail.com>
2025-05-21 19:29:57 -0500

    Create slide carrousel

---
+++ src/components/SlideCarrousel.tsx
@@ -0,0 +1,44 @@
+import '../css/SlideCarrousel.css'
+
+export function SlideCarrousel () {
+  return (
+    <article className='slide-container'>
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+      <div className='card'>
+        <h3>Algo</h3>
+      </div>
+
+    </article>
+  )
+}
