c04ecc57c056def14e16bc4be683514f6ffd38cd
Bryan Gordon <bryanbge92@gmail.com>
2025-05-26 20:16:43 -0500

    Update component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AnimatedHeader } from './components/AnimatedHeader'
 import { BlurHover } from './components/BlurHover'
 import { Content } from './components/Content'
 import { Footer } from './components/Footer'
+import { Transition } from './components/Transition'
+import { TransitionDetail } from './components/TransitionDetail'
 // import { SlideCarrousel } from './components/SlideCarrousel'
 // import { Header } from './components/Header'
 // import { Modal } from './components/Modal'
@@ -9,13 +12,15 @@ import { Footer } from './components/Footer'
 
 function App () {
   return (
-    <main>
-      <AnimatedHeader />
-      <Content />
-      <Content />
-      <BlurHover />
-      <Footer />
-    </main>
+    <Router>
+        <main>
+        <Routes>
+          <Route path='/' element={<Transition />} />
+          <Route path='/transition-detail' element={<TransitionDetail />} />
+        </Routes>
+        <Footer />
+        </main>
+      </Router>
   )
 }
 
